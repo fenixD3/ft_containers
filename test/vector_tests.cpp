22d15895bbe6c110721cb545b21fb2c11db50e93
fenixD <filipcev2@gmail.com>
2022-01-29 00:00:21 +0300

    add tests

--- test/vector_tests.cpp
+++ test/vector_tests.cpp
@@ -7,7 +7,7 @@ TEST(VectorTest, IsEmpty)
     ASSERT_TRUE(v1.empty());
 }
 
-TEST(VectorTest, EqualWithStd)
+TEST(VectorTest, EqualEmpty)
 {
     ft::vector<int> v1;
     std::vector<int> v2;
@@ -15,8 +15,18 @@ TEST(VectorTest, EqualWithStd)
     ASSERT_TRUE(ft::equal(v1.begin(), v1.end(), v2.begin()));
 }
 
-//int main()
-//{
-//    ::testing::InitGoogleTest();
-//    return RUN_ALL_TESTS();
-//}
+TEST(VectorTest, ConstEqualEmpty)
+{
+    const ft::vector<int> v1;
+    const std::vector<int> v2;
+    ASSERT_EQ(v1.size(), v2.size());
+    ASSERT_TRUE(ft::equal(v1.begin(), v1.end(), v2.begin()));
+}
+
+TEST(VectorTest, Equal)
+{
+    std::vector<int> std_vec = { 1, 2, 3};
+    ft::vector<int> ft_vec(std_vec.begin(), std_vec.end());
+    ASSERT_EQ(std_vec.size(), ft_vec.size());
+    ASSERT_TRUE(ft::equal(ft_vec.begin(), ft_vec.end(), std_vec.begin()));
+}
